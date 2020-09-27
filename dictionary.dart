637b69c9ab0b13f8c88569cc6f5d06927c9be4a5
eibarrolasdam1 <eibarrolasdam1@gmail.com>
2020-09-27 11:06:11 +0200

    "Clase dictionary hecha con servicio REST"

--- dictionary.dart
+++ dictionary.dart
@@ -9,6 +9,7 @@ class Dictionary {
    * La palabra devuelta debe ser aleatoria.
    */
   String makeRandomWord() {
-    return "bullseye"; // stub
+    String palabraAleatoria = httpGet('https://random-word-api.herokuapp.com/word')
+    return palabraAleatoria;
   }
 }

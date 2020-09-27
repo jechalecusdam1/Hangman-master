7acf657e84efc2957f5b724213176a53311d2aad
rgonzalezrdam1 <rgonzalezrdam1@gmail.com>
2020-09-27 12:08:33 +0200

    Ejercicio 6 terminado

--- txtprocessor.dart
+++ txtprocessor.dart
@@ -45,7 +45,11 @@ class TxtProcessor {
     var missedLetters = "";
     // stub (está mal, incorpora todas las letras, era para ver cambios)
     for (int i = 0; i < letterTrialList.length; i++) {
-      missedLetters = "$missedLetters ${letterTrialList[i]}";
+        if (isLetterInWord(letterTrialList[i], keyword) == false) {
+        missedLetters += letterTrialList[i];
+        missedLetters += " ";
+      }
+      //missedLetters = "$missedLetters ${letterTrialList[i]}";
     }
     return missedLetters;
   }
